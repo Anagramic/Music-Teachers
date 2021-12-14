8de63698f30249f78db493f5aebff80877735c3b
James <anagrmaic.james@gmail.com>
2021-12-14 17:44:48 +0000

    added password atribute to create acount

--- create.php
+++ create.php
@@ -4,5 +4,5 @@
 
 <form action="create_backend.php" method="post">
     Email: <input type="text" name="email"><br>
-    Password: <input type="text" name="password"><br>
+    Password: <input type="password" name="password"><br>
     <input type="submit">
